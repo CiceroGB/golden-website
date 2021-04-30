447db7c430a271e4d669682661bd4e9696ad9c25
Cicero Gasparini Brunelli <brunelli.cg@gmail.com>
2021-04-30 18:28:32 -0300

    Navbar styles

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,14 +1,87 @@
+import { useState, useEffect } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
-import { Nav, NavLogo, NavIcon } from './styles';
+import { Button } from '../../styles/global';
+
+import {
+    Nav,
+    NavLogo,
+    NavIcon,
+    NavContainer,
+    MobileIcon,
+    NavMenu,
+    NavItem,
+    NavLinks,
+    NavItemBtn,
+    NavBtnLink
+} from './styles';
 
 export function Navbar() {
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+
+    useEffect(() => {
+        showButton();
+    }, []);
+
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    };
+
+
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
     return (
         <>
             <Nav>
-                <NavLogo to="/" >
-                    <NavIcon />
-                    ULTRA
-                </NavLogo>
+                <NavContainer>
+                    <NavLogo to="/" >
+                        <NavIcon />
+                        ULTRA
+                    </NavLogo>
+                    <MobileIcon onClick={handleClick}>
+                        {click ? <FaTimes /> : <FaBars />}
+                    </MobileIcon>
+                    <NavMenu onClick={handleClick} click={click}>
+                        <NavItem>
+                            <NavLinks to='/' onClick={closeMobileMenu}>
+                                Home
+                            </NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='/services' onClick={closeMobileMenu}>
+                                Services
+                            </NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='/products' onClick={closeMobileMenu}>
+                                Products
+                            </NavLinks>
+                        </NavItem>
+                        <NavItemBtn>
+                            {button ? (
+                                <NavBtnLink to='/sign-up'>
+                                    <Button primary>SIGN UP</Button>
+                                </NavBtnLink>
+                            ) : (
+                                <NavBtnLink to='/sign-up'>
+                                    <Button onClick={closeMobileMenu} fontBig primary>
+                                        SIGN UP
+                                    </Button>
+                                </NavBtnLink>
+                            )}
+                        </NavItemBtn>
+                    </NavMenu>
+
+
+
+                </NavContainer>
             </Nav>
 
 
